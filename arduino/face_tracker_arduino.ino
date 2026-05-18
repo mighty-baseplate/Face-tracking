c048026abe78a4b2cbc9986b6025790cf97a17d9
mighty-baseplate <195085837+mighty-baseplate@users.noreply.github.com>
2026-05-18 15:37:17 +0000

    clean up arduino sketch

--- arduino/face_tracker_arduino.ino
+++ arduino/face_tracker_arduino.ino
@@ -1,6 +1,5 @@
 #include <Servo.h>
 
-// --- Configuration ---
 const int PAN_SERVO_PIN = 9;
 const int TILT_SERVO_PIN = 10;
 
@@ -15,49 +14,46 @@ const int TILT_CENTER_ANGLE = 90;
 const int ANGLE_STEP = 2;
 const long BAUD_RATE = 9600;
 
-// --- Global Variables ---
 Servo panServo;
 Servo tiltServo;
 int currentPanAngle = PAN_CENTER_ANGLE;
 int currentTiltAngle = TILT_CENTER_ANGLE;
 
-// --- Setup Function ---
 void setup() {
   Serial.begin(BAUD_RATE);
   Serial.println("Arduino Pan-Tilt Face Tracker Ready.");
-  
+
   panServo.attach(PAN_SERVO_PIN);
   tiltServo.attach(TILT_SERVO_PIN);
-  
+
   panServo.write(currentPanAngle);
   tiltServo.write(currentTiltAngle);
   delay(500);
 }
 
-// --- Loop Function ---
 void loop() {
   if (Serial.available() > 0) {
     char command = Serial.read();
-    
+
     switch (command) {
       case 'L': currentPanAngle += ANGLE_STEP; break;
       case 'R': currentPanAngle -= ANGLE_STEP; break;
       case 'U': currentTiltAngle += ANGLE_STEP; break;
       case 'D': currentTiltAngle -= ANGLE_STEP; break;
-      case 'C': break; // Center - No horizontal movement
-      case 'S': break; // Still - No vertical movement
-      case 'H': 
+      case 'C': break;
+      case 'S': break;
+      case 'H':
         currentPanAngle = PAN_CENTER_ANGLE;
         currentTiltAngle = TILT_CENTER_ANGLE;
         break;
     }
-    
+
     currentPanAngle = constrain(currentPanAngle, PAN_MIN_ANGLE, PAN_MAX_ANGLE);
     currentTiltAngle = constrain(currentTiltAngle, TILT_MIN_ANGLE, TILT_MAX_ANGLE);
-    
+
     panServo.write(currentPanAngle);
     tiltServo.write(currentTiltAngle);
-    
+
     delay(20);
   }
 }
