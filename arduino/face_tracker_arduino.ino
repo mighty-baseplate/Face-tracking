5eb6547ff1a5c00ec13f7b51018ce836f241e3db
mighty-baseplate <195085837+mighty-baseplate@users.noreply.github.com>
2026-05-18 15:20:38 +0000

    clean up arduino sketch comments

--- arduino/face_tracker_arduino.ino
+++ arduino/face_tracker_arduino.ino
@@ -1,6 +1,5 @@
 #include <Servo.h>
 
-// --- Configuration ---
 const int PAN_SERVO_PIN = 9;
 const int TILT_SERVO_PIN = 10;
 
@@ -15,13 +14,11 @@ const int TILT_CENTER_ANGLE = 90;
 const int ANGLE_STEP = 2;
 const long BAUD_RATE = 9600;
 
-// --- Global Variables ---
 Servo panServo;
 Servo tiltServo;
 int currentPanAngle = PAN_CENTER_ANGLE;
 int currentTiltAngle = TILT_CENTER_ANGLE;
 
-// --- Setup Function ---
 void setup() {
   Serial.begin(BAUD_RATE);
   Serial.println("Arduino Pan-Tilt Face Tracker Ready.");
@@ -34,7 +31,6 @@ void setup() {
   delay(500);
 }
 
-// --- Loop Function ---
 void loop() {
   if (Serial.available() > 0) {
     char command = Serial.read();
@@ -44,8 +40,8 @@ void loop() {
       case 'R': currentPanAngle -= ANGLE_STEP; break;
       case 'U': currentTiltAngle += ANGLE_STEP; break;
       case 'D': currentTiltAngle -= ANGLE_STEP; break;
-      case 'C': break; // Center - No horizontal movement
-      case 'S': break; // Still - No vertical movement
+      case 'C': break;
+      case 'S': break;
       case 'H': 
         currentPanAngle = PAN_CENTER_ANGLE;
         currentTiltAngle = TILT_CENTER_ANGLE;
